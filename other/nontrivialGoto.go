630768912a466747404ba7c656cc36f7e99c87e3
zoroqi <zhangqi560832@gmail.com>
2020-08-01 12:59:17 +0800

    goto

---
+++ other/nontrivialGoto.go
@@ -0,0 +1,61 @@
+package other
+
+type Status int
+
+type Token struct {
+	tok string
+}
+
+const (
+	ACCEPT Status = 1
+	ERROR  Status = 0
+)
+
+var END = Token{tok: "END"}
+
+func gettoken() Token {
+	return Token{}
+}
+
+func shift(t Token) bool {
+	return false
+}
+
+func reduce(t Token) bool {
+	return false
+}
+
+func forParse() Status {
+	var tok Token
+	for {
+		tok = gettoken()
+		if tok == END {
+			return ACCEPT
+		}
+		for !shift(tok) {
+			if !reduce(tok) {
+				return ERROR
+			}
+		}
+	}
+}
+
+
+func gotoParse() Status {
+
+	var tok Token
+reading:
+	tok = gettoken()
+	if tok == END {
+		return ACCEPT
+	}
+shifting:
+	if shift(tok) {
+		goto reading
+	}
+	//reducing:
+	if reduce(tok) {
+		goto shifting
+	}
+	return ERROR
+}
