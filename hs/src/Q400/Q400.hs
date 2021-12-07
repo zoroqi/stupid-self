931c1eb109b810e3d5495fd5ce7ff2de3c454a4b
zoroqi <zhangqi560832@gmail.com>
2021-12-07 18:47:38 +0800

    q400

--- hs/src/Q400/Q400.hs
+++ hs/src/Q400/Q400.hs
@@ -2,17 +2,33 @@ module Q400.Q400
 (
     findNthDigitPlanA,
     findNthDigitPlanB,
-    splitNum
 ) where
 
-findNthDigitPlanA :: Int -> Integer
-findNthDigitPlanA 0 = 0
+findNthDigitPlanA :: Int -> Int
+findNthDigitPlanA n | n <= 0 = 0
 findNthDigitPlanA n = last . take n . concat . map (reverse . splitNum) $ [1..]
 
-splitNum :: Integer -> [Integer]
+splitNum :: Int -> [Int]
 splitNum 0 = []
 splitNum x = (x `mod` 10): splitNum (x `div` 10);
 
-findNthDigitPlanB :: Int -> Integer
-findNthDigitPlanB 0 = 0
-findNthDigitPlanB n = last . take n . concat . map (reverse . splitNum) $ [1..]
+findNthDigitPlanB :: Int -> Int
+findNthDigitPlanB n | n <= 0 = 0
+findNthDigitPlanB n = planB m c where
+    c = findRange n
+    m = n - (sum [ 9 * x * (pow 10 (x-1)) | x<-[0..(c-1)]])
+
+findRange :: Int -> Int
+findRange n = local n 1 where
+        local x1 x2
+            | x1 <= 0 = x2 -1
+            | otherwise = local (x1 - x2 * 9 * (pow 10 (x2-1))) (x2+1)
+
+planB :: Int -> Int -> Int
+planB m c = (splitNum num)!!index where
+        num = ((m-1) `div` c) + (pow 10 (c-1))
+        index = c - ((m-1) `mod` c) - 1
+
+pow :: Int -> Int -> Int
+pow x1 0 = 1
+pow x1 x2 = foldl (*) 1 . take x2 . repeat $ x1
